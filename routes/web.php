87cc13e462e65b46f92fe3877fe3a23ae6b2a2a9
Alvaro Gonzalez <reynel.gonzalez.40@gmail.com>
2026-05-20 19:08:45 -0500

    Serve static landing locally

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->file(base_path('index.html'));
+});
+
+Route::get('/styles.css', function () {
+    return response()->file(base_path('styles.css'), [
+        'Content-Type' => 'text/css; charset=UTF-8',
+    ]);
+});
+
+Route::get('/app.js', function () {
+    return response()->file(base_path('app.js'), [
+        'Content-Type' => 'application/javascript; charset=UTF-8',
+    ]);
 });
